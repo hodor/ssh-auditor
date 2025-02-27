f6906d9e4df06c0fa063572fdc2c496507557779
Rogério Gasi <rtgasi@gmail.com>
2025-02-27 12:24:27 -0800

    Update scan.go
    Adding timeout as an argument for scan

--- cmd/scan.go
+++ cmd/scan.go
@@ -8,12 +8,18 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var timeoutMs int
+
 var scanCmd = &cobra.Command{
 	Use:   "scan",
 	Short: "Scan hosts using new or outdated credentials",
 	Run: func(cmd *cobra.Command, args []string) {
+		// Convert timeoutMs to a time.Duration
+        	timeoutDuration := time.Duration(timeoutMs) * time.Millisecond
+		
 		scanConfig := sshauditor.ScanConfiguration{
 			Concurrency: concurrency,
+			Timeout: timeoutDuration
 		}
 		auditor := sshauditor.New(store)
 		_, err := auditor.Scan(scanConfig)
@@ -38,6 +44,7 @@ var scanResetIntervalCmd = &cobra.Command{
 }
 
 func init() {
+	scanCmd.Flags().IntVar(&timeoutMs, "timeout", 4000, "SSH connection timeout in milliseconds")
 	RootCmd.AddCommand(scanCmd)
 	scanCmd.AddCommand(scanResetIntervalCmd)
 }
